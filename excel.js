ac48a10e9ac0748e371676d42bf269857164dd7d
Muhib_17 <amuhib2003@gmail.com>
2025-02-10 12:09:48 +0600

    adding setTimeOut method

--- excel.js
+++ excel.js
@@ -1,19 +1,22 @@
 document.getElementById("tableform").addEventListener("submit",function(event){
     event.preventDefault();
     let n=parseInt(document.getElementById("number").value);
-    let tablecontainer=document.getElementById("tablecontainer");
-    tablecontainer.innerHTML="";
-if(n>0){
-    let table=document.createElement("table");
-    for(let i=0;i<n;i++){
-        let row=document.createElement("tr");
-        for(let j=0;j<n;j++){
-            let cell=document.getElementById("td");
+    let tableContainer=document.getElementById("tableContainer");
+    tableContainer.innerHTML="";
+    if(n>0){
+        let table=document.createElement("table");
+        for(let i=0;i<n;i++){
+            setTimeout(()=>{
+    
+           
+            let row=document.createElement("tr");
+        
+        for(let j=1;j<=n;j++){
+            let cell=document.createElement("td");
+            cell.innerHTML=String(i*n+j);
             row.appendChild(cell);
         }
-        table.appendChild(row);
-
+        table.appendChild(row); },i*500);
     }
-    tablecontainer.appendChild(table);
-}
-})
+    tableContainer.appendChild(table);
+                }});
